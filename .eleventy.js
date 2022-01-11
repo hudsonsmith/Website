e87ff4d25ed7d35a82b9039bf61e003123f13934
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2022-01-10 19:10:34 -0600

    Adding syntax highlighting.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (config) => {
-	config.addPassthroughCopy("./src/assets/")
+	config.addPassthroughCopy("./src/assets/");
+	config.addPlugin(syntaxHighlight);
 }
